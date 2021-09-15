7e8bf8a0eecc1d333be2049d1acc6707fa064d86
marcos675r <marcostoromunoz@gmail.com>
2021-09-15 00:26:24 -0300

    precios agregados
    agrego precios con jquery

--- JS.js/acciones.js
+++ JS.js/acciones.js
@@ -3,3 +3,10 @@ function cambiarColor(){
     document.body.style.backgroundColor = '#a9a9a9';
 
 }
+
+$( "button" ).click(function() {
+    $( "p" ).show( "slow" );
+  });
+
+ 
+  $("#miModal").modal("show");
